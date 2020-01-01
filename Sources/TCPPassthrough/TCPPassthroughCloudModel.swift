809495e4220aa7dddb03062973532d636c21db07
David Corbin <daconex@gmail.com>
2020-01-01 14:46:48 -0500

    Make init public

--- Sources/TCPPassthrough/TCPPassthroughCloudModel.swift
+++ Sources/TCPPassthrough/TCPPassthroughCloudModel.swift
@@ -14,7 +14,7 @@ public struct TCPPassthroughCloudModel: Codable {
     var local_user_lon: Float
     var signal_strength: Int
     
-    init(local_user_uid: String, local_username: String, local_user_lat: Float, local_user_lon: Float, signal_strength: Int) {
+    public init(local_user_uid: String, local_username: String, local_user_lat: Float, local_user_lon: Float, signal_strength: Int) {
         self.local_user_uid = local_user_uid
         self.local_username = local_username
         self.local_user_lat = local_user_lat
