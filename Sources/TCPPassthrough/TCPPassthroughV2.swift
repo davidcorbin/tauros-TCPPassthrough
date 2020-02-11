75b4fbd99d6cfb0fab125763aa7cc157fdb5268c
David Corbin <daconex@gmail.com>
2020-02-11 11:14:24 -0500

    Test data

--- Sources/TCPPassthrough/TCPPassthroughV2.swift
+++ Sources/TCPPassthrough/TCPPassthroughV2.swift
@@ -139,7 +139,11 @@ public class TCPPassthroughV2 {
                     return
                 }
                 
+                print(readData.hexEncodedString())
+                
                 readData = readData.subdata(in: 0..<bytesRead)
+                
+                print(readData.hexEncodedString())
                 
                 updateLocalToRemoteByteCounter(numOfBytes: bytesRead)
             } catch {
@@ -176,7 +180,11 @@ public class TCPPassthroughV2 {
                     return
                 }
                 
+                print(readData.hexEncodedString())
+                
                 readData = readData.subdata(in: 0..<bytesRead)
+                
+                print(readData.hexEncodedString())
                 
                 updateRemoteToLocalByteCounter(numOfBytes: bytesRead)
             } catch {
@@ -235,5 +243,23 @@ public class TCPPassthroughV2 {
                 self.isLtoRTimerRunning = false
             })
         }
+    }
+}
+
+extension Data {
+    struct HexEncodingOptions: OptionSet {
+        let rawValue: Int
+        static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
+    }
+
+    func hexEncodedString(options: HexEncodingOptions = []) -> String {
+        let hexDigits = Array((options.contains(.upperCase) ? "0123456789ABCDEF" : "0123456789abcdef").utf16)
+        var chars: [unichar] = []
+        chars.reserveCapacity(2 * count)
+        for byte in self {
+            chars.append(hexDigits[Int(byte / 16)])
+            chars.append(hexDigits[Int(byte % 16)])
+        }
+        return String(utf16CodeUnits: chars, count: chars.count)
     }
 }
